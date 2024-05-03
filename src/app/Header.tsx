73f52d1829430c7515bbbce11ce2fb08ca3eadd9
laxman1892 <lxmnrzl@gmail.com>
2024-05-03 21:28:02 +0545

    Created navigation bar

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
-        <h1 className='text-2xl font-bold'>
+    <div className='flex justify-between items-center h-12'>
+        <h1 className='text-2xl'>
             Laxman
         </h1>
+        <div className="flex justify-around items-center gap-8">
+            <a href="#about" className="p-3 transition-all duration-300 ease-in-out hover:bg-black hover:rounded hover:text-white">About</a>
+            <a href="#projects" className="p-3 hover:bg-gray-300 rounded text-black">Projects</a>
+            <a href="/resume.pdf" target="_blank" rel="noreferrer" className="p-3 bg-[tomato] hover:bg-red-500 rounded text-white">Resume</a>
+        </div>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Header from "./Header";
 
 export default function Home() {
   return (
-    <div className="mx-4">
+    <div className="h-screen mx-4">
       <Header />
     </div>
   );
