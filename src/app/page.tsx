fe4df46c5cd08f0ce80d8eee53a22cc4e247975e
laxman1892 <lxmnrzl@gmail.com>
2024-05-07 14:10:43 +0545

    width changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Header from "./Header";
 
 export default function Home() {
   return (
-    <div className="h-screen py-[200px] text-center md:flex md:flex-col md:justify-center md:text-left md:my-0 md:mx-auto md:w-[742px] md:py-0 lg:w-[982px]">
+    <div className="h-screen py-[200px] text-center md:flex md:flex-col md:justify-center md:text-left md:my-0 md:mx-auto md:w-[742px] md:py-0 lg:w-[800px]">
       <h1 className="leading-[1]">HI, MY NAME IS</h1>
       <h1 className="text-8xl font-bold leading-[1]">LAXMAN RIJAL</h1>
       <h1 className="text-5xl leading-[1.2] font-bold">I BUILD THINGS FOR THE WEB.</h1>
