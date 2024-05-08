8ef9f53fed1489412c58d43db4ebc793c46cfc74
laxman1892 <lxmnrzl@gmail.com>
2024-05-08 14:32:16 +0545

    initialized a new file

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+export default function Projects() {
+    return (
+        <div className="pt-16 px-6">
+            <h1 className="text-2xl font-extrabold">
+                PROJECTS
+            </h1>
+        </div>
+    )
+}
