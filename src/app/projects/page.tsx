5fd17ed87de517ea2c2d073ac2baed10c668bd5f
laxman1892 <lxmnrzl@gmail.com>
2024-05-09 11:05:14 +0545

    streak maintainance

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,6 @@
 export default function Projects() {
     return (
+        // comments
         <div className="pt-16 px-6">
             <h1 className="text-2xl font-extrabold">
                 PROJECTS
