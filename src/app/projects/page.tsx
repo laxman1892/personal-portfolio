070f04f6095939b0b1b76781fab6c0a4362faeb0
laxman1892 <lxmnrzl@gmail.com>
2024-05-11 18:02:35 +0545

    css changes

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,7 @@
 export default function Projects() {
     return (
         // comments
-        <div className="pt-16 px-6">
+        <div className="pt-16 px-6 md:px-0 md:pt-20 md:w-[742px] lg:w-[800px] md:my-0 md:mx-auto">
             <h1 className="text-2xl font-extrabold">
                 PROJECTS
             </h1>
