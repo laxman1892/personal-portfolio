90ee207b4e24f42bf1ce44c098f547ceed3882a0
laxman1892 <lxmnrzl@gmail.com>
2024-05-14 18:54:16 +0545

    new file creation

---
+++ src/app/Projects.tsx
@@ -0,0 +1,10 @@
+export default function Projects() {
+    return (
+        // comments
+        <div className="pt-16 px-6 md:px-0 md:pt-20 md:w-[742px] lg:w-[800px] md:my-0 md:mx-auto">
+            <h1 className="text-2xl font-extrabold">
+                PROJECTS
+            </h1>
+        </div>
+    )
+}
